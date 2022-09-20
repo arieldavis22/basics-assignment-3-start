b6da2951785be59ec656f3982689531883e0dc52
Ariel Davis <amanichai100@gmail.com>
2022-09-20 17:57:48 -0400

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'basics-assignment-3-start';
-}
+export class AppComponent {}
