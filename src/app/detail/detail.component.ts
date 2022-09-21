7c6d868a492f8a5c13bf3e0a4226c2fe42ef5d0f
Ariel Davis <amanichai100@gmail.com>
2022-09-21 13:11:12 -0400

    other answer

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -16,10 +16,11 @@ export class DetailComponent implements OnInit {
 
   onClick(e) {
     this.canSee = !this.canSee;
-    this.clicks.push(this.clicks.length + 1);
+    // this.clicks.push(this.clicks.length + 1);
+    this.clicks.push(new Date());
   }
 
   getColor(num) {
-    return num >= 5 ? 'blue' : '';
+    return num >= 4 ? 'blue' : '';
   }
 }
