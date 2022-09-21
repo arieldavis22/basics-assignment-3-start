6872a022c74588f5fa929e38a6cfddffb3e58e59
Ariel Davis <amanichai100@gmail.com>
2022-09-21 11:38:43 -0400

    answer

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
+  canSee = false;
+  clicks = [];
+  count = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick(e) {
+    this.count++;
+    const num = this.count;
+    if (this.canSee) this.canSee = false;
+    else this.canSee = true;
+    this.clicks.push(num);
   }
-
 }
